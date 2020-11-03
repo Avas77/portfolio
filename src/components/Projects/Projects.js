02a536611291ed7e59c0226ca10c3ffee63539f1
Avas77 <insanebuzz09@gmail.com>
2020-11-03 10:05:27 +0545

    doing projects section

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -9,38 +9,52 @@ import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
+import covid from "../../assets/covid.png";
 
 const Projects = () => {
   return (
     <div className="projects">
       <h1 className="projects__heading">Projects</h1>
-      <Grid container spacing={2}>
-        <Grid item xs = {4}>
+      <Grid container spacing={2} className="projects__cards">
+        <Grid item xs={4}>
           <Card>
             <CardActionArea>
               <CardMedia
                 component="img"
                 alt="Contemplative Reptile"
-                height="140"
-                image="/static/images/cards/contemplative-reptile.jpg"
+                height="350"
+                image={covid}
                 title="Contemplative Reptile"
               />
               <CardContent>
                 <Typography gutterBottom variant="h5" component="h2">
-                  Lizard
+                  Fight Covid
                 </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  Lizards are a widespread group of squamate reptiles, with over
-                  6,000 species, ranging across all continents except Antarctica
+                <Typography
+                  variant="body2"
+                  color="textSecondary"
+                  component="p"
+                  className="projects__cards-desc"
+                >
+                  Web Application that raises awareness about the current global
+                  pandemic. It records number of deaths, infected and recovered
+                  people around the world. It has google maps functionality.
                 </Typography>
+                <div className="projects__cards-tools">
+                  <span>React.Js</span>
+                  <span className="extra">Materialize CSS</span>
+                  <span>Github</span>
+                </div>
               </CardContent>
             </CardActionArea>
-            <CardActions>
+            <CardActions className="projects__btn">
               <Button size="small" color="primary">
-                Share
+                <span className="txt">Source Code</span>
+                <span className="keys">&lt; &gt;</span>
               </Button>
               <Button size="small" color="primary">
-                Learn More
+                <span className="txt">Live Demo</span>
+                <span className="keys">&#8599;</span>
               </Button>
             </CardActions>
           </Card>
